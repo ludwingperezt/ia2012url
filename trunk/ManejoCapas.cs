f870a5ab590faccf00053ff2f2f522150fed4acf
aadriochoa@gmail.com <aadriochoa@gmail.com@1d01deda-5263-f3a6-e556-2408b5f3fc56>
2012-04-07 16:04:32 +0000

    arreglo del metodo agregarCapa

--- trunk/ManejoCapas.cs
+++ trunk/ManejoCapas.cs
@@ -19,8 +19,8 @@ public class ManejoCapas
     //atributos privados
     private List<Capas> capas; //Son cada una de las capas de la Red
     private List<Double> salidaEsperada; //las salidas que se esperan...
-    //private List<Perceptron> capaAnterior; //padre, enlace a la capa anterior...
-    //private List<Perceptron> capaSiguiente; //hijos, enlace a la capa siguiente
+    private Capas ultima = new Capas();// para la  ultima capa
+    
 
     
 
@@ -34,31 +34,21 @@ public class ManejoCapas
    public void agregarCapa(int nNeuronas)
    {
        Capas capanueva = new Capas(); // realizo una instancia de la clase Capas.
-       Perceptron padre= new Perceptron();// se crea el perceptron padre de la lista
-       capanueva.AgregarNeurona(padre);// se agrega el padre de la lista 
-       for (int i = 0; i < nNeuronas ; i++)//agrego el resto de neuronas
+       ultima = capas[capas.Count - 1]; //asigno la  ultima capa 
+       for (int i = 0; i < nNeuronas ; i++)//agrego las neuronas de la nueva capa
        {
            Perceptron perc = new Perceptron();
-           capanueva.AgregarNeurona(perc);
-       }
-       this.capas.Add(capanueva);// hasta aqui  solo se a agregado la capa nueva sin aver enlazado la salida
-       //de la anterior con  la entrada de la recien agregada
-       if (capas.Count != 1) // comparamos para ver si es la capa inicial, donde la entrada es la que le coloquemos
-       {
-           int index = this.capas.Count - 1;// esta es la capa actual la que acamos de ingresar 
-           //se maneja la capa anterior a la que ingresamos por eso se le vuelve a restar 1, y asi se realiza la conexion
-           this.capas[index].Entradas = this.capas[index - 1].ObtenerSalidas(this.capas[index - 1].Entradas);
-           //guardo las salidas ya que el anterior  las genera pero no las guarda en otro atributo, es algo redundante pero sirve.
-           this.capas[index].Salidas = this.capas[index].ObtenerSalidas(this.capas[index].Entradas); 
-
-       }
-       else
-       {
-           this.capas[0].Entradas = entradas; // se concidera la capa inicial por lo que se le asignan los pesos del inicio.
+           for (int m = 0; m < ultima.NumeroNeuronas(); m++)//le asigno a cada padre su nuevo hijo
+           {
+               Perceptron padre = ultima.GetNeurona(m); //asigno la neurona padre de la capa anterior
+               padre.AgregarHijo(perc);//le agrego su hijo   al padre de la capa anterior
+               perc.AgregarPadre(padre);// le agrego el padre al perceptron que estoy por agregar a la nueva capa
+           }          
+          
+           capanueva.AgregarNeurona(perc);// agrego el perceptron hijo a la nueva capa
        }
 
-
-            
+       capas.Add(capanueva);  //ya que se le ah ingresado todos los perceptrones a la capa esta se agrega a la lista.          
    }  
 
    
